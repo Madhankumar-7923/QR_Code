f93d3fe1ced2f2bf01f6ff55d8bf1b5709d7aa30
Madhankumar-7923 <madhankumar13148@gmail.com>
2024-10-15 18:43:25 +0530

    made dir change

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
 import qr from "qr-image";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
 
+// Get the directory name from the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
+
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
